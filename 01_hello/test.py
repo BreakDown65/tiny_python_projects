b416b7fe631cd605bd75ea6fe5e65d213c3fad9f
Somoskői Gábor <somoskoi.gabor@gmail.com>
2026-06-19 17:17:14 +0200

    Windowsra idomítva

--- 01_hello/test.py
+++ 01_hello/test.py
@@ -4,48 +4,34 @@
 import os
 from subprocess import getstatusoutput, getoutput
 
-prg = './hello.py'
+prg = 'hello.py'
+run = f'python {prg}'
 
 
-# --------------------------------------------------
 def test_exists():
-    """exists"""
-
     assert os.path.isfile(prg)
 
 
-# --------------------------------------------------
 def test_runnable():
-    """Runs using python3"""
-
-    out = getoutput(f'python3 {prg}')
+    out = getoutput(run)
     assert out.strip() == 'Hello, World!'
 
 
-# --------------------------------------------------
 def test_executable():
-    """Says 'Hello, World!' by default"""
-
-    out = getoutput(prg)
+    out = getoutput(run)
     assert out.strip() == 'Hello, World!'
 
 
-# --------------------------------------------------
 def test_usage():
-    """usage"""
-
     for flag in ['-h', '--help']:
-        rv, out = getstatusoutput(f'{prg} {flag}')
+        rv, out = getstatusoutput(f'{run} {flag}')
         assert rv == 0
         assert out.lower().startswith('usage')
 
 
-# --------------------------------------------------
 def test_input():
-    """test for input"""
-
     for val in ['Universe', 'Multiverse']:
         for option in ['-n', '--name']:
-            rv, out = getstatusoutput(f'{prg} {option} {val}')
+            rv, out = getstatusoutput(f'{run} {option} {val}')
             assert rv == 0
             assert out.strip() == f'Hello, {val}!'
